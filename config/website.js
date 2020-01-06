012028d3b4d5d61d99234bf3cc2fc3a401d6f198
Christopher Ueda <chrisu@benefitcosmetics.com>
2020-01-06 12:07:36 -0800

    Update website with mixed plate info

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
+  siteTitle: 'Mixed Plate Bay Area Hawaiian Dragon Boat Team', // Navigation and Site Title
+  siteTitleAlt: 'Dragon Boat Hawaii', // Alternative Site title for SEO
+  siteTitleShort: 'Mixed Plate Blog', // short_name for manifest
   siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteDescription: 'Information about the Bay Area Dragon Boat Team',
+  author: 'Linda Ueda', // Author for schemaORGJSONLD
+  organization: '',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@lindaueda', // Twitter Username
+  ogSiteName: 'Dragon Boat Blog', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
 
@@ -21,8 +21,8 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  // twitter: 'https://twitter.com/lindaueda/',
+  // twitterHandle: '@lindaueda',
+  // github: 'https://github.com/lindaueda/',
   linkedin: '',
 }
